fc9191614dd5333b7ac94751cc20e178cdb1917e
J0B4st1an <jhoan-917@outlook.es>
2025-11-29 17:36:37 -0500

    cambios para ver css

--- ClickCar/settings.py
+++ ClickCar/settings.py
@@ -116,12 +116,11 @@ USE_TZ = True
 # STATIC & MEDIA FILES
 # ===========================
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 if not DEBUG:
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
+
+
+
+
